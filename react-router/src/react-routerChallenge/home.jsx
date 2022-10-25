3113f296b32e82369694cd2eb87a87994630be1f
Taivnaa032 <taivnaajostar@Taivanbayars-MacBook-Pro.local>
2022-10-25 20:27:33 +0800

    built css

--- react-router/src/react-routerChallenge/home.jsx
+++ react-router/src/react-routerChallenge/home.jsx
@@ -1,75 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import backImg from "./backImg.jpg";
-
-const style = {
-  containerImg: {
-    backgroundImage: `url(${backImg})`,
-    width: "100vw",
-    height: "100vh  ",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-  },
-  nav: {
-    display: "flex",
-    justifyContent: "space-around",
-    margintTop: "10px",
-    alignItems: "center",
-  },
-  links: {
-    textDecoration: "none",
-    color: "white",
-    width: "60px",
-  },
-  logo: {
-    fontFamily: "Mulish",
-    color: "white",
-    fontWeight: "800",
-    fontSize: "40px",
-    alignItems: "center",
-    textAlign: "center",
-    marginLeft: "-20%",
-  },
-  navLeft: {
-    display: "flex",
-    gap: "10%",
-    marginRight: "-20%",
-  },
-  butTop: {
-    width: "200px",
-    height: "60%",
-    opacity: "0.52",
-    backgroundColor: "none",
-    border: "solid 3px white",
-  },
-};
+import "./style.css"
 
 export const Home = () => {
   return (
-    <div className="container" style={style.containerImg}>
-      <div style={style.nav}>
-        <p style={style.logo}>team</p>
-        <div style={style.navLeft}>
-          <Link style={style.links}>Products</Link>
-          <Link style={style.links}>Services</Link>
-          <Link style={style.links}>Contact</Link>
-          <Link style={style.links}>Log in</Link>
-          <button style={style.butTop}>Get Access</button>
+    <div className="container">
+      <nav>
+        <div className="nav-left">
+          <Link className="top-logo">team</Link>
+          <div className="top-rec"></div>
         </div>
-      </div>
+        <div className="top-right">
+          <Link className="linked-navs">Products</Link>
+          <Link className="linked-navs">Services</Link>
+          <Link className="linked-navs">Contact</Link>
+          <Link className="linked-navs">Log in</Link>
+          <Link className="spe-trt">Get Access</Link>
+        </div>
+      </nav>
+
       <main>
-        <article style={style.article}>
-          Instant collaborations for remote teams
+        <article >
+          Instant collaborations <br />
+          for remote teams
         </article>
-        <section style={style.section}>
-          All in one for your remote team chats, collaboration and track
+        <section >
+          All in one for your remote team chats,
+          <br /> collaboration and track
           projects
         </section>
-        <div>
+        <div className="main-top">
           <input placeholder="Email" />
-          <button>Get early access</button>
+          <button className="but-bot">Get early access</button>
         </div>
       </main>
     </div>
   );
 };
+
+
+export default Home;
