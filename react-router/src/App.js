8b65357172d2e3a05f40016bc3d977442519a546
Taivnaa032 <etaivnaa032@gmail.com>
2022-10-28 17:30:46 +0800

    china game ee hiigrei, stopwatch hiisn

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,43 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Card from "./chinaGame/card"
 
-const App = () => {
-  const [milsecond, setMilsecond] = useState(0);
-  const [second, setSecond] = useState(0);
-  const [minut, setMinut] = useState(0);
-  let interval;
-  const Start = () => {
-    interval =
-      useEffect(() => {
-        setInterval(() => {
-          setMilsecond(milsecond + 1)
-        }, 1000);
-        if (milsecond === 100) setSecond(second + 1)
-        if (second === 60) setMinut(minut + 1)
-      }, [milsecond, second, minut])
-  }
-  const Stop = () => {
-    clearInterval(interval)
-  }
-
-
-  return (
+const App = () =>{
+  return(
     <div className="App">
-      <div className="App-header" style={{ display: "flex", gap: "30px" }}>
-        <div>00</div>
-        <div>:</div>
-        <div>{minut}</div>
-        <div>:</div>
-        <div>{second}</div>
-        <div>:</div>
-        <div>{milsecond}</div>
-      </div>
-      <div style={{ display: "flex", gap: "30px" }}>
-        <button onClick={Start()}>START</button>
-        <button onClick={Stop()}>STOP</button>
-        <button>RESET</button>
+      <div className="screen">
+        <Card />
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default App;
+
--- react-router/src/stopwatch/task.jsx
+++ react-router/src/stopwatch/task.jsx
@@ -1,1 +1,55 @@
-import React from "react";
+
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [number, setNumber] = useState(0);
+  const [count, setCount] = useState(false);
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  useEffect(() => {
+    if (count) {
+      const interval = setInterval(() => {
+        setNumber((pre) => pre + 1);
+      }, 10);
+      if (number === 100) {
+        setNumber(0);
+        setSecond(second + 1);
+        if (second === 60) {
+          setSecond(0);
+          setMinute(minute + 1);
+        }
+      }
+      return () => clearInterval(interval);
+    }
+  }, [count, number, second, minute]);
+
+  return (
+    <div className="">
+      <div className="App-header" style={{ display: "flex", gap: "30px" }}>
+        <div>0</div>
+        <div>:</div>
+        <div>{minute}</div>
+        <div>:</div>
+        <div>{second}</div>
+        <div>:</div>
+        <div>{number}</div>
+      </div>
+      <div style={{ display: "flex", gap: "30px" }}>
+        <button onClick={() => setCount(true)}>START</button>
+        <button onClick={() => setCount(false)}>STOP</button>
+        <button
+          onClick={() => {
+            setMinute(0);
+            setSecond(0);
+            setNumber(0);
+            setCount(0);
+          }}
+        >
+          RESET
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
