f9202d6f3f93f32c2b8fba6226d80c8c04cd3db7
Taivnaa032 <taivnaajostar@Taivanbayars-MacBook-Pro.local>
2022-11-06 23:28:47 +0800

    changed b-color

--- mui/src/components/Header.jsx
+++ mui/src/components/Header.jsx
@@ -2,24 +2,25 @@ import React from "react";
 import { Container, AppBar, Box, Typography } from "@mui/material";
 
 export const Header = () => (
-  <AppBar>
+  <AppBar sx={{backgroundColor:"white"}}>
     <Container sx={{backgroundColor:"white"}}>
-      <Box sx={{ display: "flex", justifyContent: "space-between"}}>
+      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems:"center"}}>
         <Typography 
         variant="h5"
         sx={{
             color:"black",
         }}>team.</Typography>
         <Box sx={{ display: "flex", gap: 3 }}>
-          <Typography>Products</Typography>
-          <Typography>Services</Typography>
-          <Typography>Contact</Typography>
-          <Typography>Log in</Typography>
-          <Typography>Get Access</Typography>
+          <Typography sx={{color:"gray"}}>Products</Typography>
+          <Typography sx={{color:"gray"}}>Services</Typography>
+          <Typography sx={{color:"gray"}}>Contact</Typography>
+          <Typography sx={{color:"gray"}}>Log in</Typography>
+          <Typography sx={{color:"gray"}}>Get Access</Typography>
         </Box>
       </Box>
     </Container>
-  </AppBar>
+  </AppBar> 
 );
 
 export default Header;
+        
