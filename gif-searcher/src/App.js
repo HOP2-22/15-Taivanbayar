fa645dbb3a70cefa3e181279ae0c338f6f06c0e1
Taivnaa032 <etaivnaa032@gmail.com>
2022-11-21 19:17:49 +0800

    done giphy

--- gif-searcher/src/App.js
+++ gif-searcher/src/App.js
@@ -1,48 +1,54 @@
-import logo from "./logo.svg";
 import axios from "axios";
 import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [posts, setPost] = useState();
-
-  const gitContainer = axios.create({
-    baseURL:
-      "https://api.giphy.com/v1/stickers/search?api_key=AFkVcLnDM8r6ki73bSr1mh0kLpOlDN5C&q=2&limit=25&offset=0&rating=g&lang=en",
-  });
+  const [posts, setPost] = useState([]);
+  const [print, setPrint] = useState("");
 
   useEffect(() => {
+    console.log(print);
+    const gitContainer = axios.create({
+      baseURL: `https://api.giphy.com/v1/gifs/search?api_key=AFkVcLnDM8r6ki73bSr1mh0kLpOlDN5C&q=${print}&limit=25&offset=0&rating=g&lang=en`,
+    });
     const fetchGif = async () => {
       try {
         const gif = await gitContainer.get("/");
         setPost(gif.data.data);
-        console.log(gif.data.data);
       } catch (error) {
         setPost("Page isn't working properly");
       }
     };
     fetchGif();
-  }, []);
+  }, [print]);
 
+  console.log(posts);
   return (
-    <div className="App">
-      <input type={"search"} />
-      {posts?.map((post, index) => {
-        return <GifPrinter post={post} index={index} />;
-      })}
+    <div
+      className="App"
+      style={{ marginTop: "1vh", gap: " 2vh"}}
+    >
+      <div>
+      <input
+        type={"search"}
+        onChange={(e) => {
+          e.preventDefault();
+          setPrint(e.target.value);
+        }}
+        />
+        </div>
+      <div  >
+          {posts?.map((post) => {
+            return (
+              <div>
+                <p>{post.title}</p>
+                <img src={post.images.fixed_height.url} />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
-
-export const GifPrinter = ({ post, index }) => (
-  <div style={{ display: "flex", gap:"10px" }}>
-    <p>{index}</p>
-    <p>{post.title}</p>
-    <img src={post.bitly_url} style={{width:"150px", height:"150px"}}/>
-    <div>
-    {post.bitly_url}
-    </div>
-  </div>
-);
 
 export default App;
--- project/src/components/CardPost.jsx
+++ project/src/components/CardPost.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Grid } from "@mui/material";
+import { Box, CircularProgress, Grid } from "@mui/material";
 import Card from "./Card";
 import { ColorModeContext } from "../context/ThemeContext";
 
@@ -7,21 +7,32 @@ export const AllCard = () => {
   
   const {instance} = useContext(ColorModeContext)
   const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
+        setLoading(true);
         const res = await instance.get("/");
         setPost(res.data.data);
       } catch (error) {
         setPost("Page isn't working")
       }
+      setLoading(false);
     };
     fetchPosts();
   }, []);
   return (
     <Grid spacing={4} container>
-      {post?.map((el) => {
+      
+      { loading ? (
+          <Box sx={{ display: "flex", width: "50%", height: "50vh", alignItems:"center" }}>
+            <p style={{fontSize:"5vh"}}>Loading ...</p>
+            <CircularProgress/>
+          </Box>
+      ) : (
+      post?.map((el) => {
         return (
+          
           <Grid item sm={12} xs={12} md={6} xl={4}>
             <Card
               name={el.owner.firstName + " " + el.owner.lastName}
@@ -35,7 +46,7 @@ export const AllCard = () => {
             />
           </Grid>
         );
-      })}
+      }))}
     </Grid>
   );
 };
