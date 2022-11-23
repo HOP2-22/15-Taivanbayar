21bf75c3d9adf51f85e5439e85159012c29046fe
Taivnaa032 <etaivnaa032@gmail.com>
2022-11-23 19:38:15 +0800

    interceptor requesting

--- gif-searcher/src/App.js
+++ gif-searcher/src/App.js
@@ -3,63 +3,82 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [posts, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [print, setPrint] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  const loader = () => {
+    console.log(loading);
+    return loading === true ? <p>Loading ...</p> : "";
+  };
+
+  document.title = "Gif Searcher";
+  const fetchGif = async () => {
+    const gif = await axios.get(
+      `https://api.giphy.com/v1/gifs/search?api_key=AFkVcLnDM8r6ki73bSr1mh0kLpOlDN5C&q=${print}&limit=25&offset=0&rating=g&lang=en`
+    );
+    console.log(gif, "gif");
+    setPosts(gif);
+  };
 
   useEffect(() => {
-    const gitContainer = axios.create({
-      baseURL: `https://api.giphy.com/v1/gifs/search?api_key=AFkVcLnDM8r6ki73bSr1mh0kLpOlDN5C&q=${print}&limit=25&offset=0&rating=g&lang=en`,
-    });
-    document.title= "Gif Searcher"
-    const fetchGif = async () => {
-      try {
-        const gif = await gitContainer.get("/");
-        setPost(gif.data.data);
-      } catch (error) {
-        setPost("Page isn't working properly");
-      }
+    const reqInterceptor = (request) => {
+      setLoading(true);
+      return request;
     };
-    fetchGif();
-  }, [print]);
 
-  console.log(posts);
+    const pendInterceptor = axios.interceptors.request.use(
+      reqInterceptor,
+      (err) => console.log("error")
+    );
+
+    const resInterceptor = (response) => {
+      return response.data.data;
+    };
+    const interceptor = axios.interceptors.response.use(
+      resInterceptor,
+      (err) => {
+        console.log("error");
+      }
+    );
+    setLoading(false);
+    return () =>
+      axios.interceptors.response.eject(interceptor, pendInterceptor);
+  }, []);
   return (
-      <div
-      className="App"
-      style={{ marginTop: "1vh", gap: " 2vh"}}
-    >
+    <div className="App" style={{ marginTop: "1vh", gap: " 2vh" }}>
       <div>
-      <input
-        type={"search"}
-        onChange={(e) => {
-          e.preventDefault();
-          setPrint(e.target.value);
-        }}
-        />
-        </div>
-      <div  >
-          {posts?.map((post) => {
-            return (
-              <div>
-                <p>{post.title}</p>
-                <img src={post.images.fixed_height.url} />
-              </div>
-            );
-          })}
+        <input type={"search"} onChange={(e) => setPrint(e.target.value)} />
+        <button
+          onClick={() => {
+            fetchGif();
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div>
+        {loading === true
+          ? {}
+          : posts?.map((post) => {
+              return (
+                <div>
+                  <p>{post.title}</p>
+                  <img src={post.images.fixed_height.url} />
+                </div>
+              );
+            })}
       </div>
     </div>
   );
 }
 
 export const GifPrinter = ({ post, index }) => (
-  <div style={{ display: "flex", gap:"10px" }}>
+  <div style={{ display: "flex", gap: "10px" }}>
     <p>{index}</p>
     <p>{post.title}</p>
-    <img src={post.bitly_url} style={{width:"150px", height:"150px"}}/>
-    <div>
-    {post.bitly_url}
-    </div>
+    <img src={post.bitly_url} style={{ width: "150px", height: "150px" }} />
+    <div>{post.bitly_url}</div>
   </div>
 );
 
--- instapage/src/App.js
+++ instapage/src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
-import menu  from "./images/menu.png"
-import pause from "./images/pause.png"
-import play from "./images/play-button.png"
-import mute from "./images/mute.png"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import menu from "./images/menu.png";
+import pause from "./images/pause.png";
+import play from "./images/play-buttton.png";
+import mute from "./images/mute.png";
+import story from "./images/prince.jpeg";
+import avatar from "./images/princess.jpeg";
 
 function App() {
+  const [playLogo, setPlay] = useState(`${play}`);
+
+  useEffect(()=> {
+    
+  }, [])
+
   return (
     <div
       className="App"
@@ -19,29 +29,63 @@ function App() {
     >
       <div
         style={{
-          width: "30%",
+          width: "45%",
           height: "95vh",
           borderRadius: "30px",
-          backgroundImage: "white",
+          backgroundImage: `url(${story})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "15px",
         }}
       >
         <div
           style={{
-            width: "80%",
-            height: "1vh",
-            borderRadius: "5px",
-            background: "blue",
+            display: "flex",
+            marginTop: "2vh",
+            width: "90%",
           }}
-        ></div>
-        <div>
-          <div>
-            <img />
-            <p></p>
+        >
+          <div
+            className="processing-div"
+            style={{
+              width: "100%",
+              height: "1vh",
+              borderRadius: "5px",
+            }}
+          ></div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "90%",
+          }}
+        >
+          <div style={{ display: "flex", gap: "15px" }}>
+            <img
+              src={avatar}
+              style={{ width: "45px", height: "45px", borderRadius: "50%" }}
+            />
+            <p>taivnaa</p>
+            <p>3h ago</p>
           </div>
-          <div>
-            <img src={play}/>
-            <img src={mute}/>
-            <img src={menu}/>
+
+          <div style={{ display: "flex", gap: "20px" }}>
+            <div 
+            className="playBut"
+            onClick={()=> {
+              setPlay(
+              playLogo === play ? pause : play
+              )
+            }}>
+              <img src={playLogo} style={{ width: "25px", height: "25px" }} />
+            </div>
+            <img src={mute} style={{ width: "25px", height: "25px" }} />
+            <img src={menu} style={{ width: "25px", height: "25px" }} />
           </div>
         </div>
       </div>
