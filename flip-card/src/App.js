4497b337d45897327725152e66e2831ce680d30f
Taivnaa032 <etaivnaa032@gmail.com>
2022-12-05 17:27:06 +0800

    started flip card

--- flip-card/src/App.js
+++ flip-card/src/App.js
@@ -1,29 +1,67 @@
-import "./App.css";
 import { Box, Container } from "@mui/system";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { Button } from "@mui/material";
 
-import Images from "./assets/imgCollection.jsx"
-import cardCover from "./images/card-image.jpeg";
+import { Images } from "./assets/imgCollection";
+import CardCover from "./assets/images/card-image.jpeg";
+// function * chunks(arr, n) {
+//   for (let i = 0; i < arr.length; i += n) {
+//     yield arr.slice(i, i + n);
+//   }
+// }
 
-
+const Card = ({ image }) => {
+  const [rotate, setRotate] = useState(false);
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "300px",
+        width: "300px",
+      }}
+    >
+      {rotate ? (
+        <img
+          src={CardCover}
+          alt="cardcover"
+          style={{
+            width: "300px",
+          }}
+          onClick={() => {
+            setRotate(true);
+          }}
+        />
+      ) : (
+        <img
+          alt="image"
+          src={image.src}
+          style={{
+            width: "300px",
+          }}
+          onClick={() => {
+            setRotate(false);
+          }}
+        />
+      )}
+    </Box>
+  );
+};
 
 const CardContainer = () => {
   const [cardPost, setCardPost] = useState(Array(10).fill(false));
-  const [some, setSome] = useState([])
+  const [images, setImage] = useState([]);
+  const generate = () => {
+    let newImages = [...Images, ...Images].sort((a, b) => Math.random() - 0.5);
+    setImage(newImages);
+  };
 
-  useEffect(()=>{
-      setSome(Images.sort(Images.map((image)=>{
-       image = Math.random() - 0.5
-    })))
-  }, [])
-  
   return (
     <Box>
       <Container>
-        {cardPost.map(() => {
-          
+        {images.map((image, index) => {
+          return <Card image={image} index={index} key={index} />;
         })}
+        <Button onClick={() => generate()}>generate</Button>
       </Container>
     </Box>
   );
